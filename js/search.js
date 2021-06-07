cd474ded217f73f126b5d89169d329dbc9fa350d
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-06-07 14:18:34 -0400

    finishing search

--- js/search.js
+++ js/search.js
@@ -1,15 +1,26 @@
+
 const filter = document.getElementById("employee-filter");
 const employeeList = document.getElementsByClassName("name");
 
 
-const handleSearch = event =>{
-    userInput = event.target.value.toLowerCase();
-    employeeList.forEach(employeeName => {
-        console.log(employeeName);
+const handleSearch = e =>{
+    userInput = e.target.value.toLowerCase();
+    Array.from(employeeList).forEach(employeeNames => {
+        employeeName = employeeNames.textContent.toLowerCase();
+        nameCard = employeeNames.parentNode.parentNode;
+        console.log(nameCard);
+        if(employeeName.includes(userInput)){
+            nameCard.style.display="box";
+        }else{
+            nameCard.style.display="none"
+        }
+        
 
     })
     
 }
+
+
 
 filter.addEventListener("keyup", handleSearch);
 
