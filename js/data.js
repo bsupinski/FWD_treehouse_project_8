556b150f6f2961a4c7cf46fff7d46cc1a91a18d0
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-06-07 23:36:19 -0400

    working on employee navigating

--- js/data.js
+++ js/data.js
@@ -1,10 +1,12 @@
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,
+const urlAPI = `https://randomuser.me/api/?results=45&inc=name,picture,
 email,location,phone,dob&noinfo&nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
 //Fetch API
 async function getJson(url){
@@ -53,34 +55,48 @@ getEmployees(urlAPI)
 function displayModal(index){
     let {name, dob, phone, email, location:{city, street, state, postcode}, picture} = employees[index];
     let date = new Date(dob.date);
-    console.log(employees);
     const modalHTML = `
-    <img class="avatar" src="${picture.large}" alt="emplyee #1">
-    <div class="text-container">
-        <h2 class="name">${name.first} ${name.last}</h2>
-        <p class="email">${email}</p>
-        <p class="address">${city}</p>
-        <hr />
-        <p class="phone-number">${phone}</p>
-        <p class="address">${street.number} ${street.name}, ${state}, ${postcode}</p>
-        <p class="birthday">Birthday: ${date.getMonth()}/${date.getDay()}/${date.getFullYear()}</p>
-    </div>
+        <img class="avatar" src="${picture.large}" alt="${name.first} ${name.last}">
+        <div class="text-container">
+            <h2 class="modalname">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p class="phone-number">${phone}</p>
+            <p class="address">${street.number} ${street.name}, ${state}, ${postcode}</p>
+            <p class="birthday">Birthday: ${date.getMonth()}/${date.getDay()}/${date.getFullYear()}</p>
+        </div>
     `;
 
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
 }
 
-
 gridContainer.addEventListener("click", e =>{
     if(e.target !==gridContainer){
         const card = e.target.closest(".card");
-        const index = card.getAttribute("data-index");
+        let index = card.getAttribute("data-index");
         displayModal(index);
     }
 })
 
+
 modalClose.addEventListener("click", () => {
     overlay.classList.add("hidden")
+})
+
+previous.addEventListener("click", () => {
+    //if displayModal(index)==0
+        //displaymodal(index+employees.length)
+    //else change displayModal(index-1)
+    
+
 
 })
+
+next.addEventListener("click", () => {
+    //if displayModal(index)==employee.length
+        //displaymodal(index)==0
+    //else change displayModal(index+1)
+})
+
