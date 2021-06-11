5f0e7bf250f3c942da3047724f151937bc7ec6e4
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-06-11 16:38:50 -0400

    set # back to 40

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,
+const urlAPI = `https://randomuser.me/api/?results=40&inc=name,picture,
 email,location,phone,dob&noinfo&nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector(".overlay");
